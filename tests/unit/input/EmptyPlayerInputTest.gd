3c9b2b8c76700e02843cf722e5e674365fd74966
myin <minyin142p@gmail.com>
2021-04-22 19:18:49 +0200

    Update test with ui inputs

--- tests/unit/input/EmptyPlayerInputTest.gd
+++ tests/unit/input/EmptyPlayerInputTest.gd
@@ -8,8 +8,8 @@ func before_each():
 
 
 func test_no_inputs():
-	empty_input.handle_input(press_key("move_right"))
-	assert_false(empty_input.is_pressed("move_right"))
+	empty_input.handle_input(press_key("ui_right"))
+	assert_false(empty_input.is_pressed("ui_right"))
 
 	empty_input.handle_input(joypad_motion_event(JOY_AXIS_0, 1))
-	assert_eq(empty_input.get_action_strength("move_right"), 0.0)
+	assert_eq(empty_input.get_action_strength("ui_right"), 0.0)
--- tests/unit/input/InputReaderTest.gd
+++ tests/unit/input/InputReaderTest.gd
@@ -8,38 +8,38 @@ func before_each():
 
 
 func test_single_input():
-	input.handle_input(press_key("move_left"))
-	assert_true(input.is_pressed("move_left"))
+	input.handle_input(press_key("ui_left"))
+	assert_true(input.is_pressed("ui_left"))
 
-	input.handle_input(release_key("move_left"))
-	assert_false(input.is_pressed("move_left"))
+	input.handle_input(release_key("ui_left"))
+	assert_false(input.is_pressed("ui_left"))
 
 
 func test_multiple_input():
-	input.handle_input(press_key("move_left"))
-	input.handle_input(press_key("move_right"))
+	input.handle_input(press_key("ui_left"))
+	input.handle_input(press_key("ui_right"))
 
-	assert_true(input.is_pressed("move_left"))
-	assert_true(input.is_pressed("move_right"))
+	assert_true(input.is_pressed("ui_left"))
+	assert_true(input.is_pressed("ui_right"))
 
-	input.handle_input(release_key("move_right"))
-	assert_true(input.is_pressed("move_left"))
-	assert_false(input.is_pressed("move_right"))
+	input.handle_input(release_key("ui_right"))
+	assert_true(input.is_pressed("ui_left"))
+	assert_false(input.is_pressed("ui_right"))
 
 
 func test_action_strength():
-	input.handle_input(press_key("move_left", 1))
-	assert_eq(input.get_action_strength("move_left"), 1.0)
+	input.handle_input(press_key("ui_left", 1))
+	assert_eq(input.get_action_strength("ui_left"), 1.0)
 
 	input.handle_input(joypad_motion_event(JOY_AXIS_0, 1))
-	assert_eq(input.get_action_strength("move_right"), 1.0)
+	assert_eq(input.get_action_strength("ui_right"), 1.0)
 
 	assert_eq(input.get_action_strength("ui_up"), 0.0)
 
 
 func test_just_pressed():
-	input.handle_input(press_key("move_left"))
-	assert_true(input.is_just_pressed("move_left"))
+	input.handle_input(press_key("ui_left"))
+	assert_true(input.is_just_pressed("ui_left"))
 
 	simulate(input, 1, 1)
-	assert_false(input.is_just_pressed("move_left"))
+	assert_false(input.is_just_pressed("ui_left"))
