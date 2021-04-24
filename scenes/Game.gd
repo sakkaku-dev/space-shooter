88e696d5908e9cffcdfbc0259a0cf0a38abf82ae
myin <minyin142p@gmail.com>
2021-04-24 12:10:51 +0200

    Enable cursor after gameover

--- scenes/Game.gd
+++ scenes/Game.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
 
+onready var spawner = $SpawnerRandom/Spawner
+
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+
+func _on_PlayerShip_died():
+	spawner.set_spawn(false)
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
